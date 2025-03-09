5e21a84038bdb34ae54aa4f1c74a91590580f5bc
Marmik <marmikpatel246@gmail.com>
2025-03-09 15:42:20 +0530

    Servlet Making

---
+++ Project_01/src/main/java/basics01/FirstServlet.java
@@ -0,0 +1,29 @@
+package basics01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest req, HttpServletResponse rs) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		rs.setContentType("text/html");
+		PrintWriter out  = rs.getWriter();
+		
+		out.println("<h1>Hello , There is the first Web-App</h1>");
+		out.close();
+	}
+
+}
